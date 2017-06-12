ce3f66318ed9ebe54340e33fecd86a91fc1d3cbe
razoowp <razoowp@gmail.com>
2017-06-12 10:33:03 +0545

    Two ways to reverse and print list item

--- reverselist.py
+++ reverselist.py
@@ -7,3 +7,11 @@ print(list[::-1])
 mylist = [9,0,5,6,34,5]
 for i in reversed(mylist):
     print(i)
+
+
+# If we want to keep the reverse list as new list we need to code like this:
+mylist = [9,0,5,6,34,5]
+newlist=[]
+for i in reversed(mylist):
+    newlist.append(i)
+print(newlist)
